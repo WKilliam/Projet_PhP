e1bab722650f39f2736ddc04d408d4efb5332904
Tania <tania.rojas@edu.itescia.fr>
2020-01-22 13:36:42 +0100

    début

---
+++ Projet_PHP.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="Code.css">
+<head>
+	<title></title>
+</head>
+<body>
+<header>
+<img id="logo" src="logo.jpg" />
+<div class="menu">
+<input class="burger" type="checkbox">
+    <nav>
+        <a href="#">1.<i class="material-icons">Team</i>Membre</a>
+        <a href="#">2.<i class="material-icons">Production du Projet</i>Production</a>
+        <a href="#">3.<i class="material-icons">Base de Donnée</i>Data</a>
+    </nav>
+    </div>
+</header>
+ <ul>
+    <li>Yuyuan Wu</li>
+    <li>Teddy Villars</li>
+    <li>Tania Rojas</li>
+</ul>
+<p>Avancement du projet</p>
+<p>Détail de l'avancement<q/p>
+
+<footer>
+    <span class="title">Team Production</span>
+</footer>
+</body>
+</html>
