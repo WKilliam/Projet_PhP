1cd27b4f9bb920b3806cae45a42855c7e2f182a2
Tania <tania.rojas@edu.itescia.fr>
2020-01-24 12:34:21 +0100

    BDD ENREGISTRE!!!!!!!!

--- page_laposte.php
+++ page_laposte.php
@@ -10,10 +10,6 @@ require "src/Request.php";
 require "src/Formulaire.php";
  ?>
 <?php
-// On crée notre array $coordonnees
-$contacts= array(array('Nom'=>'Wu','Prenom'=>'Yuyuan','DateNaissance'=>'12/34/56','Sexe'=>'F','Mail'=>'yw@ei.fr','Adresse'=>'kjhl'),
-array('Nom'=>'Villars','Prenom'=>'Teddy','DateNaissance'=>'23/45/67','Sexe'=>'M','Mail'=>'tv@ei.fr','Adresse'=>'hjgk'),
-array('Nom'=>'Rojas','Prenom'=>'Tania','DateNaissance'=>'34/56/78','Sexe'=>'F','Mail'=>'tr@ei.fr','Adresse'=>'ljhkjh'));
 
 ?>
 
@@ -21,12 +17,13 @@ array('Nom'=>'Rojas','Prenom'=>'Tania','DateNaissance'=>'34/56/78','Sexe'=>'F','
 	<?php 
 	$bdd = new Request('mysql', 'localhost', 'personnes', 'root', 'root');
 	$bdd->getmybdd();
-	$contab=$bdd->getAllRow('personnes');
+	//$contab=$bdd->getAllRow('Contacts');
 	?>
 	<FORM method= "post">
 	<?php 
 	$formu = new Formulaire();
-	echo $formu->dropdownList('Destinataire',$contacts[$Nom]).'<br><br>';
+
+	echo $formu->dropdownList('Destinataire',$contab[$Nom]).'<br><br>';
 	echo $formu->dropdownList('Expéditeur',$contacts[$Nom]).'<br><br>';
 	echo $formu->checkbox('Timbre Prioritaire').'<br>';
 	echo $formu->checkbox('Confidentiel');
--- src/Request.php
+++ src/Request.php
@@ -65,7 +65,7 @@ class Request {
      */
   public function getAllRow($myTable){
     foreach($this->bdd->query('SELECT * FROM '. $myTable) as $row) {
-      return $row;
+      print_r ($row);
     }
   }
 
@@ -82,8 +82,7 @@ class Request {
      * @return bool
      */
   public function setInsert($nom, $prenom, $naissance, $sexe, $mail, $adresse){
-      $query="INSERT INTO `Contacts`("."`Nom`, `Prenom`, `DateNaissance`, `Sexe`, `Mail`, `Adresse`".") VALUES (".$nom.", ".$prenom.", ".$naissance.", ".$sexe.", ".$mail.", ".$adresse.")";
-      //INSERT INTO `Contacts` (" . "`nom`, `prenom`, `naissance`, `sexe`, `mail`, `adresse` ".") VALUES ('".$nom . "', '".$prenom . "', '".$naissance."', '".$sexe . "', '".$mail . "', '".$adresse."')";
+      $query="INSERT INTO `Contacts`("."`Nom`, `Prenom`, `DateNaissance`, `Sexe`, `Mail`, `Adresse`".") VALUES ('".$nom."', '".$prenom."', '".$naissance."', '".$sexe."', '".$mail."', '".$adresse."')";
       // pour comprendre à quoi sert une requête préparé et son execution : http://php.net/manual/fr/pdo.prepared-statements.php
       $req = $this->bdd->prepare($query);
       // echo $query;
